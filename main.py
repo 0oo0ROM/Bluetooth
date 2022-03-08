8b1e678a9abd44c9f9c4f6a0cad2f86f4da7d0a2
Zhengyang <zhengyang.h@outlook.com>
2022-03-09 00:01:17 +0100

    simple bluetooth client and server

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-from Server import server
+#socket reference
+#https://docs.python.org/3.3/library/socket.html
 
+from Server import server
+from Client import client
+
+#server setting
 server('40:1C:83:B7:F6:D2',4)
+#client setting
+client('40:1C:83:B7:F6:D2',4)

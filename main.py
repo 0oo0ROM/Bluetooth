188e6dcb28e5d3eb10ddf0d5e21491289e2a4b2d
Zhengyang <zhengyang.h@outlook.com>
2022-03-10 16:21:42 +0100

    1.1

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 
 from Server import server
 from Client import client
+import _thread
 
+_thread.start_new_thread(server('40:1C:83:B7:F6:D2',4))
+_thread.start_new_thread(client('40:1C:83:B7:F6:D2',4))
 #server setting
-server('40:1C:83:B7:F6:D2',4)
+#server('40:1C:83:B7:F6:D2',4)
 #client setting
-client('40:1C:83:B7:F6:D2',4)
+#client('40:1C:83:B7:F6:D2',4)
